1219c0897d7b9170fd9aa5b75b900e6dfc1c4bde
Ronney <ronneydouglas1@gmail.com>
2017-08-13 18:15:31 -0400

    working

--- index.js
+++ index.js
@@ -11,31 +11,32 @@ app.set("view engine", "mustache");
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-// app.use(validator());
+app.use(expressValidator());
 
-todoList = {
-    "items": [
-        {"id": 1, "item": "Wash Car", "complete": true},
-        {"id": 2, "item": "Walk Dog", "complete": false}
-    ]
-};
+let todoList = [
+   {item: "Wash Car", complete: true},
+   {item: "Walk Dog", complete: false}
+   ];
 
 app.get("/", function(req, res) {
-    res.render("list", todoList);
+    res.render("list", {todoList: todoList});
 });
 
 
 app.post("/", function(req, res) {
-    let i = todoList.items.length + 1;
-    todoList.items.push({"id": i, "item": req.body.item, "complete": false});
+    todoList.push({"item": req.body.item, complete: false});
     res.redirect("/");
 });
 
-app.get("/complete/:id", function() {
-    todoList.items[req.params.id -1].complete = true;
+app.post("/complete", function(req, res) {
+    todoList.forEach(function(pending) {
+      if(pending.item === req.body.confirm) {
+         pending.complete = true;
+      }
+   })
         res.redirect("/");
 });
 
-app.listen(3000, function() {
+app.listen(8000, function() {
     console.log("All good things must come to an end");
 });
